0b7aacf30fab910eb3d3fc4933b4ae5943118580
Walter <walter.clark24@yahoo.com>
2022-11-21 13:45:32 -0500

    commit tree 5b5039a197b8b63588a1c1cd6362ea79078cd214

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+const messages = ["Guadalupe", "Ollie", "Aki", "surprise"]
+
+
+function writeCards(messages) {
+    for (let i = 0; i < messages.length -1; i++) {
+        console.log (`'Thank You, ${messages[i]}, for the wonderful ${messages[3]} gift!'`)
+        
+       
+    }
+    
+    return messages
+}
+
+console.log (writeCards(messages))
+
+let countDown = 10
+while (countDown > -1){
+    console.log(countDown--)
+}
+  
+    
